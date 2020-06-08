d54fd7aee40a258a6d7eb32798191454bfb8c030
Leah Migacz <lmigacz@msudenver.edu>
2020-06-08 15:38:40 -0600

    fixed Capitalized problem 💸

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,22 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyAdmin = require('../lib/KeyAdmin');
+
 const key = {
-    set() {
-        console.log('set key');
+    async set() {
+        const keyAdmin = new KeyAdmin();
+        const input = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'key',
+                message: 'Enter Nomics API Key '.blue + 'https://nomics.com'
+            }
+        ]);
+
+        const key = keyAdmin.setKey(input.key);
+        if (key) {
+            console.log('API key added.');
+        }
     },
     show() {
         console.log('set show');
