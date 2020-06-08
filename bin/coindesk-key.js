e7b59f0fc6744cab603c4dd77067fbd919e4070c
Leah Migacz <lmigacz@msudenver.edu>
2020-06-08 12:57:32 -0600

    added commands 💸

--- bin/coindesk-key.js
+++ bin/coindesk-key.js
@@ -5,4 +5,14 @@ program
     .description('Set API Key -- Get at https://nomics.com')
     .action(() => console.log('fuck'));
 
+    program
+    .command('show')
+    .description('Show API Key')
+    .action(() => console.log('fuck'));
+
+    program
+    .command('remove')
+    .description('Delete API Key')
+    .action(() => console.log('fuck'));
+    
 program.parse(process.argv);
