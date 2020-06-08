255d5d86bcae36ffa721d90321116deecd7cf3ed
Leah Migacz <lmigacz@msudenver.edu>
2020-06-08 14:28:13 -0600

    commands working 💸

--- bin/coindesk-key.js
+++ bin/coindesk-key.js
@@ -1,19 +1,24 @@
-const program = require("commander");
+const program = require('commander');
 const key = require('../commands/key');
 
 program
-    .command('set')
-    .description('Set API Key -- Get at https://nomics.com')
-    .action(key.set);
+  .command('set')
+  .description('API Key --- Get one at https://nomics.com')
+  .action(key.set);
 
-    program
-    .command('show')
-    .description('Show API Key')
-    .action(key.show);
+program
+  .command('show')
+  .description('Show current API Key')
+  .action(key.show);
 
-    program
-    .command('remove')
-    .description('Delete API Key')
-    .action(key.remove);
+program
+  .command('remove')
+  .description('Remove current API Key')
+  .action(key.remove);
 
 program.parse(process.argv);
+
+// If no args, output help
+if (!process.argv[2]) {
+  program.outputHelp();
+}
--- bin/coindesk.js
+++ bin/coindesk.js
@@ -4,6 +4,6 @@ const package = require('../package.json');
 
 program
   .version(package.version)
-  .command('key', 'API Key info -- https://nomics.com')
-  .command('check', 'View current value')
+  .command('key', 'API Key --- https://nomics.com')
+  .command('check', 'View current exchange price')
   .parse(process.argv);
