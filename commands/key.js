b14bae78270676e49a4798ee6cceba95db0d4964
Leah Migacz <lmigacz@msudenver.edu>
2020-06-08 13:00:59 -0600

    added key and subs 💸

---
+++ commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+    set() {
+        console.log('set key');
+    },
+    show() {
+        console.log('set show');
+    },
+    remove() {
+        console.log('set remove');
+    }
+}
+
+module.exports = key;
