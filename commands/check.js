1c8ce54a8cbdfbf6fac8d99916bea428f1159f1f
Leah Migacz <lmigacz@msudenver.edu>
2020-06-14 21:34:14 -0600

    we have DATA!  💸

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,19 @@
+const KeyAdmin = require('../lib/KeyAdmin');
+const CoinAPI = require('../lib/CoinAPI');
+
 const check = {
-    price(cmd) {
-        console.log(cmd.coin, cmd.curr);
+    async price(cmd) {
+        try {
+            keyAdmin = new KeyAdmin();
+            const key = keyAdmin.getKey();
+
+            const api = new CoinAPI(key);
+            const priceOutputData = await api.getPriceInfo(cmd.coin, cmd.curr);
+            
+            console.log(priceOutputData);
+        } catch (error) {
+            console.log(error.message.red);
+        }
     }
 };
 
